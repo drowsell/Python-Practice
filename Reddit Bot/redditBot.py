6f56f94e6b5f2658cbfe6f3ea69a9b707b032076
Dylan Rowsell <rowselldylan@gmail.com>
2019-09-14 18:55:17 -0700

    Updated reddit bot
    Need to learn how to make more private before updating with more files

--- Reddit Bot/redditBot.py
+++ Reddit Bot/redditBot.py
@@ -1,1 +1,20 @@
 import praw
+import config
+
+def bot_login():
+	r = praw.Reddit(username = config.username,
+				password = config.password,
+				client_id = config.client_id,
+				client_secret = config.client_secret,
+				user_agent = "drowsell's Youtube video poster")
+	return r
+
+def run_bot(r):
+	for comment in r.subreddit('test').comments(limit=25):
+		if "goals" in comment.body:
+			comment.reply("test test test")
+			print "string found"
+
+
+r = bot_login()
+run_bot(r)
