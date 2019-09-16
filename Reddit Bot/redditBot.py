7006f8d9149b12cb05e6b6aca733ebf1ecffb2a5
Dylan Rowsell <rowselldylan@gmail.com>
2019-09-15 17:22:28 -0700

    still learning

--- Reddit Bot/redditBot.py
+++ Reddit Bot/redditBot.py
@@ -1,5 +1,8 @@
 import praw
 import config
+import time
+import os
+
 
 def bot_login():
 	r = praw.Reddit(username = config.username,
@@ -9,12 +12,30 @@ def bot_login():
 				user_agent = "drowsell's Youtube video poster")
 	return r
 
-def run_bot(r):
+def run_bot(r, comments_replied_to):
+
 	for comment in r.subreddit('test').comments(limit=25):
-		if "goals" in comment.body:
-			comment.reply("test test test")
+		if "goals" in comment.body and comment.id not in comments_replied_to:
+			comment.reply("test test test3")
 			print "string found"
 
+			comments_replied_to.append(comment.id)
 
+			with open ("comments_replied_to.txt", "a") as f:
+				f.write(comment.id + "\n")
+
+def get_saved_comments():
+	if not os.path.isfile("comments_replied_to.txt"):
+		comments_replied_to = []
+	else:
+		with open("comments_replied_to.txt", "r") as f:
+			comments_replied_to = f.read()
+			comments_replied_to = comments_replied_to.split("\n")
+			comments_replied_to = filter(None, comments_replied_to)
+
+	return comments_replied_to
+
+comments_replied_to = get_saved_comments()
 r = bot_login()
-run_bot(r)
+run_bot(r, comments_replied_to)
+print comments_replied_to
